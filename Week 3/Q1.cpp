1fbd81f3c9e6d5ee5710b2d6255d29b289c97bdd
Nitya Sanguri <161462356+SoulDeplane@users.noreply.github.com>
2025-03-02 11:43:39 +0530

    Q1.cpp
    Given an unsorted array of integers, design an algorithm and a  program to sort the array using insertion sort. Your program should be able to find number of comparisons and shifts ( shifts - total number of times the array elements are shifted from their place) required for sorting the array.

--- Week 3/Q1.cpp
+++ Week 3/Q1.cpp
@@ -6,7 +6,7 @@ void sort(int *arr, int n)
   	int i, j, k=0, s=0, t;
   	for(i=1; i<n; i++)
   	{
-				t=arr[i];
+		t=arr[i];
     		j=i-1;
     		while(j>=0)
     		{
@@ -28,18 +28,25 @@ void sort(int *arr, int n)
 }
 main()
 {
-  	int n, i;
-  	cout<<"Enter size: ";
-  	cin>>n;
-  	int a[n];
-  	for(i=0; i<n; i++)
-    		cin>>a[i];
-  	cout<<"Unsorted array: ";
-  	for(i=0; i<n; i++)
-    		cout<<a[i]<<" ";
+  	int n, i, t;
+	cout<<"Enter no. of test cases: ";
+	cin>>t;
+	while(t>0)
+	{
+	  	cout<<"Enter size: ";
+	  	cin>>n;
+	  	int a[n];
+	  	for(i=0; i<n; i++)
+	    		cin>>a[i];
+	  	cout<<"Unsorted array: ";
+	  	for(i=0; i<n; i++)
+	    		cout<<a[i]<<" ";
+			cout<<endl;
+	  	sort(a, n);
+	  	cout<<"Sorted array: ";
+	  	for(i=0; i<n; i++)
+	    		cout<<a[i]<<" ";
 		cout<<endl;
-  	sort(a, n);
-  	cout<<"Sorted array: ";
-  	for(i=0; i<n; i++)
-    		cout<<a[i]<<" ";
+		t--;
+	}
 }
