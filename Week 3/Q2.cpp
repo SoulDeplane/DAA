e18490c849e69d5ba73c4a5c916a98ebe21f5b5b
Nitya Sanguri <161462356+SoulDeplane@users.noreply.github.com>
2025-03-02 11:19:15 +0530

    Q2.cpp
    Given an unsorted array of integers, design an algorithm and  implement a program to sort this array using selection sort. Your program should also find number of comparisons and number of swaps required.

--- Week 3/Q2.cpp
+++ Week 3/Q2.cpp
@@ -1,3 +1,4 @@
+/*Given an unsorted array of integers, design an algorithm and  implement a program to sort this array using selection sort. Your program should also find number of comparisons and number of swaps required.*/
 #include<iostream>
 using namespace std;
 void sort(int *arr, int n)
